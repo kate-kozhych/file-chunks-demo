560ab5d96a4ce73f70cda84ffc0cb39699bd7528
Katsiaryna Kozhych <kate.kozhych@gmail.com>
2026-04-17 14:34:13 +0200

    feat: create integration tests, extract contentLength to local val to fix nullable type errors.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
 
+    testImplementation(kotlin("test"))
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
